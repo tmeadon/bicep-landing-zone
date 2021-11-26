3ce402e92feaad4dbaf3546c1c02052e9a14b583
ingi.ossurarson@newsignature.com <ingi.ossurarson@newsignature.com>
2021-11-26 10:58:28 +0000

    adding to cleanup

--- scripts/cleanup.js
+++ scripts/cleanup.js
@@ -1,7 +1,14 @@
 const fs = require('fs');
+const color = require('colors');
 const parseJson = require('parse-json');
 const jsonFilePath = './landing-zone/config/resource-groups.json';
 
 let json = parseJson(fs.readFileSync(jsonFilePath));
 
-console.log(json.resourceGroups);
+const rsgs = json.resourceGroups;
+console.log('Listing Resource Groups used in the YAML configuration'.magenta);
+rsgs.forEach((rsg) => {
+    console.log(`Resource Group `.green + rsg.name + ` SubscriptionId `.green + rsg.subscriptionId);
+});
+
+
