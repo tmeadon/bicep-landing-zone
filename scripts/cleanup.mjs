ab522f7b5b985c2333b86a4dbd59d150c04909d1
ingio <ingi.ossurarson@newsignature.com>
2021-12-02 11:50:37 +0000

    messaging sorted

--- scripts/cleanup.mjs
+++ scripts/cleanup.mjs
@@ -8,66 +8,49 @@ const jsonFilePath = './landing-zone/config/resource-groups.json';
 
 let json = parseJson(fs.readFileSync(jsonFilePath));
 const rsgs = json.resourceGroups.reverse();
-
-const loading = ora({
-    text: 'Deleting Resource Group..\n',
-    color: 'yellow',
-});
+let groupFound = true;
 
 async function rsgChk(rsg) {
-    // const loading = ora({
-    //     text: 'Checking Resource Group...\n',
-    //     color: 'cyan',
-    // });
-    // loading.start();
-
     const child = spawn('powershell', [`az group exists --name ${rsg.name} --subscription ${rsg.subscriptionId}`]);
     child.stdout.on('data', (data) => {
-        console.log(`Powershell Data:\n ${data}`);
-    });
-    child.stderr.on('data', (data) => {
-        loading.fail(`Powershell Error:\n ${data}`);
-        //process.exit(1);
-    });
-    child.on('close', async () => {
-        loading.succeed(`RSG Found`);
         deleteRSG(rsg);
     });
 }
 
 async function deleteRSG(rsg) {
-
-
-    console.log(rsg.name + ` in Subscription `.yellow + rsg.subscriptionId);
+    const loading = ora({
+        text: 'Processing..',
+        color: 'yellow',
+    });
+    loading.start();
     const child = spawn('powershell', [`az group delete --name ${rsg.name} --subscription ${rsg.subscriptionId} --yes`]);
     child.stderr.on('data', (data) => {
-        loading.fail(`Powershell Error:\n ${data}`);
-        process.exit(1);
+        groupFound = false;
     });
     child.on('close', () => {
-        loading.succeed(`Resource Group ${rsg.name} Deleted`);
+        if (groupFound) {
+            loading.succeed(`Resource Group ${rsg.name} Deleted`);
+        } else {
+            loading.fail(`${rsg.name} Not Found!`);
+        }
     });
 }
 
-
 async function cleanup() {
-
-    
-
-    console.log('Listing Resource Groups used in the YAML configuration'.magenta);
-    console.log('========================================================='.magenta);
+    console.log('Listing Resource Groups used in the configuration file'.yellow);
+    console.log('========================================================='.yellow);
     console.log(rsgs);
-    console.log('========================================================='.magenta);
-    console.log('Above Resource Groups will be deleted!'.red);
+    console.log('========================================================='.yellow);
+    console.log('\nAbove Resource Groups will be deleted!\n'.red);
+
     const answers = await inquirer.askConfirm();
-    loading.start();
+
     if (answers.confirm) {
         await rsgs.forEach((rsg) => {
-            //console.log(`Resource Group `.green + rsg.name + ` SubscriptionId `.green + rsg.subscriptionId);
             rsgChk(rsg);
         });
     } else {
-        console.log('The action was cancelled!'.red);
+        console.log('The action was cancelled!'.yellow);
         process.exit(1);
     }
 }
